cbd32ed4af0aa620e5cfe2f304bf2295ab73cf73
Ersaoktaviannn <124137772+Ersaoktaviannn@users.noreply.github.com>
2024-09-06 09:33:36 +0700

    Add files via upload

---
+++ Week 2/coba.dart
@@ -0,0 +1,18 @@
+class Car {
+  String carName = "Aston Martin";
+}
+
+/*void main() { 
+   for (int i = 0; i < 5; i++) { 
+    print('hello ${i + 1}'); 
+  } */
+ void main() {
+  String name = "Fulan";
+  int startAge = 18;
+  int endAge = 9;
+
+  for (int age = startAge; age >= endAge; age--) {
+    print("Nama saya $name, sekarang berumur $age");
+  }
+}
+
